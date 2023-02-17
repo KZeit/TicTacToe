b7f0300a0efd33d5390ab2273a3902d71fc57320
KZeit <122012987+KZeit@users.noreply.github.com>
2023-02-16 22:13:49 -0500

    Add files via upload

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.White;
+            this.A1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.A1.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.A1.Location = new System.Drawing.Point(31, 32);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(88, 77);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.White;
+            this.A3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.A3.Font = new System.Drawing.Font("Tahoma", 20.25F);
+            this.A3.Location = new System.Drawing.Point(241, 33);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(88, 77);
+            this.A3.TabIndex = 3;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.button_click);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.White;
+            this.A2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.A2.Font = new System.Drawing.Font("Tahoma", 20.25F);
+            this.A2.Location = new System.Drawing.Point(136, 32);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(88, 77);
+            this.A2.TabIndex = 4;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.White;
+            this.B2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.B2.Font = new System.Drawing.Font("Tahoma", 20.25F);
+            this.B2.Location = new System.Drawing.Point(136, 131);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(88, 77);
+            this.B2.TabIndex = 5;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.White;
+            this.B3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.B3.Font = new System.Drawing.Font("Tahoma", 20.25F);
+            this.B3.Location = new System.Drawing.Point(241, 131);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(88, 77);
+            this.B3.TabIndex = 6;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.White;
+            this.B1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.B1.Font = new System.Drawing.Font("Tahoma", 20.25F);
+            this.B1.Location = new System.Drawing.Point(31, 131);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(88, 77);
+            this.B1.TabIndex = 7;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.White;
+            this.C1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.C1.Font = new System.Drawing.Font("Tahoma", 20.25F);
+            this.C1.Location = new System.Drawing.Point(31, 227);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(88, 77);
+            this.C1.TabIndex = 8;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.White;
+            this.C2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.C2.Font = new System.Drawing.Font("Tahoma", 20.25F);
+            this.C2.Location = new System.Drawing.Point(136, 227);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(88, 77);
+            this.C2.TabIndex = 9;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.White;
+            this.C3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.C3.Font = new System.Drawing.Font("Tahoma", 20.25F);
+            this.C3.Location = new System.Drawing.Point(241, 227);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(88, 77);
+            this.C3.TabIndex = 10;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(360, 342);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool playerTurn = true;
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button xo = (Button)sender;
+
+            if (playerTurn)
+            {
+                xo.Text = "X";
+                xo.Enabled = false;
+            }
+            else
+            {
+                xo.Text = "O";
+                xo.Enabled = false;
+            }
+
+            count++;
+            playerTurn = !playerTurn;
+            checkWinner();
+        }
+
+        private void checkWinner()
+        {
+            bool winner = false;
+
+            if (A1.Text == A2.Text && A2.Text == A3.Text && !A2.Enabled ||
+                B1.Text == B2.Text && B2.Text == B3.Text && !B2.Enabled ||
+                C1.Text == C2.Text && C2.Text == C3.Text && !C2.Enabled ||
+                A1.Text == B1.Text && B1.Text == C1.Text && !B1.Enabled ||
+                A2.Text == B2.Text && B2.Text == C2.Text && !B2.Enabled ||
+                A3.Text == B3.Text && B3.Text == C3.Text && !B3.Enabled ||
+                A1.Text == B2.Text && B2.Text == C3.Text && !B2.Enabled ||
+                A3.Text == B2.Text && B2.Text == C1.Text && !B2.Enabled)
+            {
+                winner = true;
+            }
+
+            if (winner == true)
+            {
+                string youWin = "";
+
+                if (playerTurn)
+                {
+                    youWin = "O";
+                    MessageBox.Show($"{youWin} Wins!");
+                    ResetGame();
+                }
+
+                else
+                {
+                    youWin = "X";
+                    MessageBox.Show($"{youWin} Wins!");
+                    ResetGame();
+                }
+            }
+            else
+            {
+                if (count == 9)
+                {
+                    MessageBox.Show("Tie!");
+                    ResetGame();
+                }
+            }
+        }
+
+        private void ResetGame()
+        {
+            Application.Restart();
+        }
+    }
+}
